eddd4c12b099cb68f69f0e3d0f661373c7c580ce
ParkChunSo <cnsth123@gmail.com>
2019-04-09 20:56:38 +0900

    init

---
+++ src/main/java/stom/com/recommend_movie/RecommendMovieApplication.java
@@ -0,0 +1,13 @@
+package stom.com.recommend_movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendMovieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/stom/com/recommend_movie/RecommendMovieApplicationTests.java
@@ -0,0 +1,16 @@
+package stom.com.recommend_movie;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecommendMovieApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
